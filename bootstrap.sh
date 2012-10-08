4a3e45bc177d1ba763b8428e077f39212def4402
ratazzi <ratazzi.potts@gmail.com>
2012-10-08 09:33:23 +0800

    activity-log

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,26 +14,26 @@ fi
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/.oh-my-zsh
 fi
-ln -s "`pwd`/.oh-my-zsh" "$HOME/.oh-my-zsh"
-ln -s "`pwd`/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/.oh-my-zsh" "$HOME/.oh-my-zsh"
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
 
 # tmux
-ln -s "`pwd`/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$HOME/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 # git
-ln -s "`pwd`/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
 
 # python
-ln -s "`pwd`/.pystartup.py" "$HOME/.pystartup.py"
+ln -s "$HOME/.dotfiles/.pystartup.py" "$HOME/.pystartup.py"
 
 if [ ! -d ".vim/bundle/vundle" ]; then
     git clone git://github.com/gmarik/vundle.git .vim/bundle/vundle
 fi
 
 # vim
-ln -s "`pwd`/.vim" "$HOME/.vim"
-ln -s "`pwd`/.vimrc" "$HOME/.vimrc"
-ln -s "`pwd`/.vimrc" "$HOME/.gvimrc"
+ln -s "$HOME/.dotfiles/.vim" "$HOME/.vim"
+ln -s "$HOME/.dotfiles/.vimrc" "$HOME/.vimrc"
+ln -s "$HOME/.dotfiles/.vimrc" "$HOME/.gvimrc"
 vim +BundleInstall +qall
 
 echo -e "\n\033[01;32mLet's rock.\033[00m\n"
